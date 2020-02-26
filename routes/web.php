26d5bd73da6cda086efffa3637ef097297f93b4a
Peak Xin <xinyflove@sina.com>
2020-02-26 21:56:21 +0800

    修改web路由

--- routes/web.php
+++ routes/web.php
@@ -13,58 +13,58 @@
 
 Route::get('/', 'LoginController@index');
 // 注册页面
-Route::get('/register', '\App\Http\Controllers\RegisterController@index');
+Route::get('/register', 'RegisterController@index');
 // 注册行为
-Route::post('/register', '\App\Http\Controllers\RegisterController@register');
+Route::post('/register', 'RegisterController@register');
 // 登录页面
 Route::get('/login', 'LoginController@index')->name('login');
 // 登录行为
-Route::post('/login', '\App\Http\Controllers\LoginController@login');
+Route::post('/login', 'LoginController@login');
 // 登出行为
-Route::get('/logout', '\App\Http\Controllers\LoginController@logout');
+Route::get('/logout', 'LoginController@logout');
 
 Route::group(['middleware' => 'auth:web'], function (){
 
     // 文章列表页
-    Route::get('/posts', '\App\Http\Controllers\PostController@index');
+    Route::get('/posts', 'PostController@index');
     // 创建文章
-    Route::get('/posts/create', '\App\Http\Controllers\PostController@create');
-    Route::post('/posts', '\App\Http\Controllers\PostController@store');
+    Route::get('/posts/create', 'PostController@create');
+    Route::post('/posts', 'PostController@store');
     // 文章详情页
-    Route::get('/posts/{post}', '\App\Http\Controllers\PostController@show');
+    Route::get('/posts/{post}', 'PostController@show');
     // 编辑文章
-    Route::get('/posts/{post}/edit', '\App\Http\Controllers\PostController@edit');
-    Route::put('/posts/{post}', '\App\Http\Controllers\PostController@update');
+    Route::get('/posts/{post}/edit', 'PostController@edit');
+    Route::put('/posts/{post}', 'PostController@update');
     // 删除文章
-    Route::get('/posts/{post}/delete', '\App\Http\Controllers\PostController@delete');
+    Route::get('/posts/{post}/delete', 'PostController@delete');
     // 图片上传
-    Route::post('/posts/image/upload', '\App\Http\Controllers\PostController@imageUpload');
+    Route::post('/posts/image/upload', 'PostController@imageUpload');
     // 提交评论
-    Route::post('/posts/{post}/comment', '\App\Http\Controllers\PostController@comment');
+    Route::post('/posts/{post}/comment', 'PostController@comment');
     // 文章点赞
-    Route::get('/posts/{post}/zan', '\App\Http\Controllers\PostController@zan');
+    Route::get('/posts/{post}/zan', 'PostController@zan');
     // 文章取消赞
-    Route::get('/posts/{post}/unzan', '\App\Http\Controllers\PostController@unzan');
+    Route::get('/posts/{post}/unzan', 'PostController@unzan');
 
     /* 用户模块 */
     // 个人设置
-    Route::get('/user/me/setting', '\App\Http\Controllers\UserController@setting');
+    Route::get('/user/me/setting', 'UserController@setting');
     // 个人设置操作
-    Route::post('/user/me/setting', '\App\Http\Controllers\UserController@settingStore');
+    Route::post('/user/me/setting', 'UserController@settingStore');
     // 个人中心
-    Route::get('/user/{user}', '\App\Http\Controllers\UserController@show');
+    Route::get('/user/{user}', 'UserController@show');
     // 关注用户
-    Route::post('/user/{user}/fan', '\App\Http\Controllers\UserController@fan');
+    Route::post('/user/{user}/fan', 'UserController@fan');
     // 取消关注
-    Route::post('/user/{user}/unfan', '\App\Http\Controllers\UserController@unfan');
+    Route::post('/user/{user}/unfan', 'UserController@unfan');
 
     /*专题模块*/
     // 专题详情页
-    Route::get('/topic/{topic}', '\App\Http\Controllers\TopicController@show');
+    Route::get('/topic/{topic}', 'TopicController@show');
     // 专题投稿
-    Route::post('/topic/{topic}/submit', '\App\Http\Controllers\TopicController@submit');
+    Route::post('/topic/{topic}/submit', 'TopicController@submit');
 
     /*通知模块*/
     // 通知
-    Route::get('/notices', '\App\Http\Controllers\NoticeController@index');
+    Route::get('/notices', 'NoticeController@index');
 });
